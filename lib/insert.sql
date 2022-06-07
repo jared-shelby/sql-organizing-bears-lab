2c1920012827eb4f2d5bc3e54c32e36a0230ec3b
Jared Shelby <jared.shelby@yale.edu>
2022-06-07 15:43:07 -0400

    commit tree b0925bbc5c3ac3107520ed1e75298331b9f49ef2

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 21, "M", "brown", "calm", 0),
+("Rowdy", 12, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "dark brown", "Nice", 1),
+("Sargeant Brown", 19, "M", "green", "Slimy", 0),
+("Melissa", 13, "F", "dark brown", "goofy", 1),
+("Grinch", 2, "M", "black", "Grinchy", 1),
+("Wendy", 6, "F", "blue", "naive", 1),
+(null, 20, "M", "black", "aggressive", 0);
